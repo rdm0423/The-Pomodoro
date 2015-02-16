cabbc826a368c2da8920b86660907dce8136a9eb
Ross McIlwaine <rdm0423@gmail.com>
2015-02-16 15:00:30 -0700

    Create Shared Instance POTimer

---
+++ The Pomodoro/POTimer.h
@@ -0,0 +1,15 @@
+//
+//  POTimer.h
+//  The Pomodoro
+//
+//  Created by Ross McIlwaine on 2/16/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface POTimer : NSObject
+
++ (POTimer *)sharedInstance;
+
+@end
---
+++ The Pomodoro/POTimer.m
@@ -0,0 +1,25 @@
+//
+//  POTimer.m
+//  The Pomodoro
+//
+//  Created by Ross McIlwaine on 2/16/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "POTimer.h"
+
+@implementation POTimer
+
++ (POTimer *)sharedInstance {
+    static POTimer *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [POTimer new];
+    });
+    return sharedInstance;
+    
+}
+
+
+
+@end
