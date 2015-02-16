be69194ce007305a7c33788f1fac782d1ea5264c
Ross McIlwaine <rdm0423@gmail.com>
2015-02-16 14:56:10 -0700

    Step 2: Add interface to the TimerViewController

--- The Pomodoro/TimerViewController.m
+++ The Pomodoro/TimerViewController.m
@@ -9,6 +9,8 @@
 #import "TimerViewController.h"
 
 @interface TimerViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) IBOutlet UIButton *timerButton;
 
 @end
 
@@ -16,22 +18,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    
+    
+    
+    
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)timerButton:(id)sender {
+    
+    
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
